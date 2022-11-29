4e40a00f14c3ee8acc7bade8cbd08d8317daee09
clzlol <nyc0527@naver.com>
2022-11-29 10:02:46 +0900

    대략적인 주석

--- app.py
+++ app.py
@@ -8,22 +8,26 @@ kor = [] #한글뜻
 value = [] #랜덤 영단어 인덱스
 repeat = {} #틀린 단어 저장 dict
 
+
+#공부하려는 단어장 불러오기
 data = list()
 f = open("교육부_3천단어_수정분.csv", 'r')
 rea = csv.reader(f)
 for row in rea:
-    data.append(row[1:4]) #영단어, 한국어뜻, 난이도를 리스트에 저장
+    data.append(row[1:3]) #영단어와 한국어뜻을 리스트에 저장
 f.close
 
 intents = nextcord.Intents.default()
 intents.message_content = True
 
+#명령어 접두사 지정, 게이트웨이설정(?)
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+#매일 같은 시간에 단어 시험을 진행한다
 async def schedule_daily_message():
     now = datetime.datetime.now()
-    then = now.replace(hour = 00, minute = 8)
+    then = now.replace(hour = 9, minute = 57)
     wait_time = (then - now).total_seconds()
     await asyncio.sleep(wait_time)
 
@@ -39,11 +43,12 @@ async def schedule_daily_message():
         kor[i] = kor[i].split(", ")
     #print(kor)
     
-    channel = bot.get_channel(1042374278424829955)
+    channel = bot.get_channel(1042374278424829955) #봇이 단어시험을 진행할 채널 지정
 
     await channel.send(f"오늘의 단어입니다.\n{words[0]}, {words[1]}, {words[2]}")
 
 
+#정답 입력하는 명령어 (ex: !정답 답1 답2 답3)
 @bot.command(name = "정답")
 async def answer(ctx, one:str, two:str, three:str):
     answer = [one, two, three]
@@ -66,6 +71,7 @@ async def answer(ctx, one:str, two:str, three:str):
         await ctx.send(f"{wrong_count}개 틀렸습니다.")
 
 
+#틀린 단어를 복습하기 위해 영단어와 한글뜻을 출력시키는 명령어 (ex: !복습)
 @bot.command(name = "복습")
 async def rework(ctx):
     repeat_output="다음 단어들을 틀렸어요.\n"
@@ -81,7 +87,8 @@ async def rework(ctx):
     f.close()
 
 
-@bot.command(name="주간기록") #최근 7일간 복습한 단어 출력
+#최근 7일간 복습한 단어 출력 (ex: !주간기록)
+@bot.command(name="주간기록") 
 async def recall(ctx):
     repeat_data = list()
     f = open("틀린_단어.csv", 'r')
@@ -104,11 +111,12 @@ async def recall(ctx):
     await ctx.send(report_output)
 
 
+#봇이 준비되는 이벤트가 발생하면 다음 명령어를 실행한다
 @bot.event
 async def on_ready():
     print(f"Logged in as: {bot.user.name}")
     await schedule_daily_message()
 
-    
+#봇 실행
 if __name__ == '__main__':
     bot.run("MTA0NDEwNTQ4NTYyNTg2MDEzNw.GVYEnF.wq_fniuyKiERUOhNkhOV7IztJZfbPrVE_YLxnU")
